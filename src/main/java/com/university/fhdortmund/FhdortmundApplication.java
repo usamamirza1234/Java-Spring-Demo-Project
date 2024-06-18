8d894c47d80111fbe001c33ed69ad67ad461849a
Usama Khalid Mirza <usamak.mirza@gmail.com>
2024-06-18 17:09:09 +0200

    Setup project

---
+++ src/main/java/com/university/fhdortmund/FhdortmundApplication.java
@@ -0,0 +1,13 @@
+package com.university.fhdortmund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhdortmundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhdortmundApplication.class, args);
+	}
+
+}
